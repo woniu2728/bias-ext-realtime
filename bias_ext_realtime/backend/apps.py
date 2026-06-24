00ba0595fb527f9dc8b2f80822c32590280e240d
woniu <2463396298@qq.com>
2026-06-24 12:14:29 +0800

    Add apps.py + django_migrations

---
+++ bias_ext_realtime/backend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class RealtimeExtensionConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bias_ext_realtime.backend"
+    label = "realtime"
